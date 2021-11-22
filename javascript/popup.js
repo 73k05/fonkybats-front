6efbd1752429a54ed136c9b3cf1dc654b663a01f
AdrienNicod <ad.nicod@gmail.com>
2021-11-22 10:27:44 +0100

    prevent Body from scrolling when modal is opened

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,10 +1,14 @@
 // SHOW MODAL 
+
+const modalBody = document.querySelector("body");
+
 const showModal = (openButton, modalContent) =>{
   const openBtn = document.getElementById(openButton),
   modalContainer = document.getElementById(modalContent)
   
   if(openBtn && modalContainer){
       openBtn.addEventListener('click', ()=>{
+          modalBody.style.overflow = "hidden";
           modalContainer.classList.add('show-modal')
       })
   }
@@ -16,6 +20,7 @@ const closeBtn = document.querySelectorAll('.close-modal')
 
 function closeModal(){
   const modalContainer = document.getElementById('modal-container')
+  modalBody.style.overflow = "auto";
   modalContainer.classList.remove('show-modal')
 }
 closeBtn.forEach(c => c.addEventListener('click', closeModal))
